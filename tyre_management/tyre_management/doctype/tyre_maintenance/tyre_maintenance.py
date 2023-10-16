de75f6d78a97ada0f523181b7c5641e7e92edcab
NaviN <navin@aerele.in>
2023-10-16 10:48:52 +0530

    Tyre Maintenance API

--- tyre_management/tyre_management/doctype/tyre_maintenance/tyre_maintenance.py
+++ tyre_management/tyre_management/doctype/tyre_maintenance/tyre_maintenance.py
@@ -3,48 +3,68 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import get_site_path,now
+import base64
+from frappe.model.naming import parse_naming_series
 
 class TyreMaintenance(Document):
 	def on_submit(self):
 		if not self.time_stamp:
 			self.time_stamp = frappe.utils.now()
-	def validate(self):
+	def after_insert(self):
 		if self.vehicle_no:
-			if not self.vehicle_tire_position:
-				tyre_position = frappe.get_value("Vehicle Tire Position",{"ref_doctype":self.ref_doctype,
-																			"docstatus" : 1,
-																			"vehicle_no":self.vehicle_no},"name")
-				self.vehicle_tire_position = tyre_position
+			if self.attach_img_byte:
+				if not self.attach_img_extension:
+					frappe.throw("Please mention the extension of the attachment")
+				byte = self.attach_img_byte
+				img_details = (parse_naming_series(f'{self.attach_img_name}-.#####') or now()) + "." + self.attach_img_extension
+				path = f"{get_site_path()}/public/files/" + img_details
+				decodeit = open(path, 'wb')
+				decodeit.write(base64.b64decode(byte))
+				decodeit.close()
+				file_doc = frappe.new_doc("File")
+				file_doc.file_url = "/files/"+img_details
+				file_doc.is_private = 0
+				file_doc.attached_to_doctype = self.doctype
+				file_doc.attached_to_name = self.name
+				file_doc.insert(ignore_permissions=True)
+				self.attachment = file_doc.file_url
+	def validate(self):
+		if not self.vehicle_tire_position:
+			tyre_position = frappe.get_value("Vehicle Tire Position",{"ref_doctype":self.ref_doctype,
+																		"docstatus" : 1,
+																		"vehicle_no":self.vehicle_no},"name")
+			self.vehicle_tire_position = tyre_position
 
-			if self.vehicle_tire_position == tyre_position:
-				position_doc = frappe.get_doc("Vehicle Tire Position",{"name":self.vehicle_tire_position})
-				serial_no_fields = [
-					"front_left_1",
-					"front_right_1",
-					"middle_left_1",
-					"middle_right_1",
-					"middle_left_2",
-					"middle_right_2",
-					"middle_left_3",
-					"middle_right_3",
-					"middle_left_4",
-					"middle_right_4",
-					"rear_left_1",
-					"rear_right_1",
-					"rear_left_2",
-					"rear_right_2",
-					"rear_left_3",
-					"rear_right_3",
-					"rear_left_4",
-					"rear_right_4",
-					"spare_1",
-					"spare_2"
-				]
-				for field in serial_no_fields:
-					if position_doc.get(field)==self.serial_no:
-						self.tire_position = field
-			else:
-				frappe.throw("This is not the latest tyre psition for this vehicle")
+		if self.vehicle_tire_position == tyre_position:
+			position_doc = frappe.get_doc("Vehicle Tire Position",{"name":self.vehicle_tire_position})
+			serial_no_fields = [
+				"front_left_1",
+				"front_right_1",
+				"middle_left_1",
+				"middle_right_1",
+				"middle_left_2",
+				"middle_right_2",
+				"middle_left_3",
+				"middle_right_3",
+				"middle_left_4",
+				"middle_right_4",
+				"rear_left_1",
+				"rear_right_1",
+				"rear_left_2",
+				"rear_right_2",
+				"rear_left_3",
+				"rear_right_3",
+				"rear_left_4",
+				"rear_right_4",
+				"spare_1",
+				"spare_2"
+			]
+			for field in serial_no_fields:
+				if position_doc.get(field)==self.serial_no:
+					self.tire_position = field
+		else:
+			frappe.throw("This is not the latest tyre psition for this vehicle")
 
 @frappe.whitelist()
 def get_latest_tyre_position_for_vehicle(doctype, vehicle_no):
